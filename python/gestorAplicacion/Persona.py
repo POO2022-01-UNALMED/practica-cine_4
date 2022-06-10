565f8d38ba87586ecc284d7eb4cb4f45c41e59e1
Kevin1875 <94397214+Kevin1875@users.noreply.github.com>
2022-06-10 14:18:20 -0500

    Add files via upload

---
+++ python/gestorAplicacion/Persona.py
@@ -0,0 +1,54 @@
+class Persona:
+    
+    def __init__(self,cedula,celular,nombre, sexo, edad):
+        self.cedula = cedula
+        self.celular = celular
+        self.nombre = nombre
+        self.sexo = sexo
+        self.edad = edad
+        
+        
+    def getCedula(self):
+        return self.cedula
+    
+    
+    def setCedula(self,cedula):
+        self.cedula=cedula
+        
+    
+    def getCelular(self):
+        return self.celular
+         
+    def setCelular(self,celular):
+        self.celular=celular
+        
+        
+        
+    def getNombre(self):
+        return self.nombre
+    
+    def setNombre(self,nombre):
+        self.nombre=nombre
+        
+        
+        
+    def getSexo(self):
+        return self.sexo
+    
+    def setSexo(self,sexo):
+        self.sexo=sexo
+        
+        
+        
+    def getEdad(self):
+        return self.edad
+    
+    def setEdad(self,edad):
+        self.edad=edad
+        
+    
+        
+        
+        
+        
+        
